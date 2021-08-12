3863960826bc137a824f9fb61fa6cea2d0e80881
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2021-08-12 07:07:00 +0200

    Code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace spoj_csharp
+{
+    class Program
+    {
+        static string halfOfHalf(string str)
+        {
+            string toReturn = "";
+            for (int i = 0; i < str.Length / 2; i += 2)
+            {
+                toReturn += str[i];
+            }
+            return toReturn;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] resource = new string[200];
+            int how_many;
+            how_many = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < how_many; i++)
+            {
+                resource[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < how_many; i++)
+            {
+                string output = halfOfHalf(resource[i]);
+                Console.WriteLine($"{output}");
+            }
+
+        }
+    }
+}
